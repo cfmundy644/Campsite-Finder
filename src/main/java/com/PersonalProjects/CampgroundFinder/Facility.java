aa3f2980cb9a5ab6b9bcd0c354fb90e8a5f3edee
cfmundy644 <61708411+cfmundy644@users.noreply.github.com>
2021-02-24 18:53:50 -0500

    Add files via upload

--- src/main/java/com/PersonalProjects/CampgroundFinder/Facility.java
+++ src/main/java/com/PersonalProjects/CampgroundFinder/Facility.java
@@ -15,19 +15,56 @@ public class Facility {
     private double latitude;
     private double longitude;
 
+    // following populated based on Google Place API
+    private String googName;
+    private double googRating;
+    private int googUserRatingsTotal;
+    private String googPlaceId;
+
     public Integer getId() {return id;};
     public void setId(Integer id) {this.id = id;}
 
     public String getRgFacilityId() {return rgFacilityId;}
-    public void setRgFacilityId(String rgFacilityId) {this.rgFacilityId = rgFacilityId;};
+    public void setRgFacilityId(String rgFacilityId) {this.rgFacilityId = rgFacilityId;}
 
     public String getRgFacilityName() {return rgFacilityName;}
-    public void setRgFacilityName(String rgFacilityName) {this.rgFacilityName = rgFacilityName;};
+    public void setRgFacilityName(String rgFacilityName) {this.rgFacilityName = rgFacilityName;}
 
     public double getLatitude() {return latitude;}
-    public void setLatitude(double latitude) {this.latitude = latitude;};
+    public void setLatitude(double latitude) {this.latitude = latitude;}
 
     public double getLongitude() {return longitude;}
-    public void setLongitude(double longitude) {this.longitude = longitude;};
+    public void setLongitude(double longitude) {this.longitude = longitude;}
 
+    public String getGoogName() {
+        return googName;
+    }
+
+    public void setGoogName(String googName) {
+        this.googName = googName;
+    }
+
+    public double getGoogRating() {
+        return googRating;
+    }
+
+    public void setGoogRating(double googRating) {
+        this.googRating = googRating;
+    }
+
+    public int getGoogUserRatingsTotal() {
+        return googUserRatingsTotal;
+    }
+
+    public void setGoogUserRatingsTotal(int googUserRatingsTotal) {
+        this.googUserRatingsTotal = googUserRatingsTotal;
+    }
+
+    public String getGoogPlaceId() {
+        return googPlaceId;
+    }
+
+    public void setGoogPlaceId(String googPlaceId) {
+        this.googPlaceId = googPlaceId;
+    }
 }
