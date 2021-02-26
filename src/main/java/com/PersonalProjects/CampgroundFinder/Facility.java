c28c85d9a1b611b45296ba333dd6cace79f8d375
cfmundy644 <61708411+cfmundy644@users.noreply.github.com>
2021-02-26 18:09:36 -0500

    Add files via upload

--- src/main/java/com/PersonalProjects/CampgroundFinder/Facility.java
+++ src/main/java/com/PersonalProjects/CampgroundFinder/Facility.java
@@ -1,14 +1,18 @@
 package com.PersonalProjects.CampgroundFinder;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.net.URL;
+import java.time.LocalDate;
 
 @Entity
 public class Facility implements Comparable<Facility> {
     @Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
     private String rgFacilityId; // rg = recreation.gov
     private String rgFacilityName;
@@ -25,20 +29,47 @@ public class Facility implements Comparable<Facility> {
     private double dist;
     private boolean available;
 
-    public Integer getId() {return id;};
-    public void setId(Integer id) {this.id = id;}
+    public Integer getId() {
+        return id;
+    }
 
-    public String getRgFacilityId() {return rgFacilityId;}
-    public void setRgFacilityId(String rgFacilityId) {this.rgFacilityId = rgFacilityId;}
+    ;
 
-    public String getRgFacilityName() {return rgFacilityName;}
-    public void setRgFacilityName(String rgFacilityName) {this.rgFacilityName = rgFacilityName;}
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
-    public double getLatitude() {return latitude;}
-    public void setLatitude(double latitude) {this.latitude = latitude;}
+    public String getRgFacilityId() {
+        return rgFacilityId;
+    }
 
-    public double getLongitude() {return longitude;}
-    public void setLongitude(double longitude) {this.longitude = longitude;}
+    public void setRgFacilityId(String rgFacilityId) {
+        this.rgFacilityId = rgFacilityId;
+    }
+
+    public String getRgFacilityName() {
+        return rgFacilityName;
+    }
+
+    public void setRgFacilityName(String rgFacilityName) {
+        this.rgFacilityName = rgFacilityName;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
 
     public String getGoogName() {
         return googName;
@@ -90,12 +121,120 @@ public class Facility implements Comparable<Facility> {
 
     @Override
     public int compareTo(Facility that) {
-        if (this.available && !that.available) {return -1;}
-        else if (!this.available && that.available) {return 1;}
+        if (this.available && !that.available) {
+            return -1;
+        } else if (!this.available && that.available) {
+            return 1;
+        }
 
-        if (this.dist < that.dist) {return -1;}
-        else if (this.dist == that.dist) {return 0;}
-        else {return 1;}
+        if (this.dist < that.dist) {
+            return -1;
+        } else if (this.dist == that.dist) {
+            return 0;
+        } else {
+            return 1;
+        }
     }
 
+    public void callAvailabilityAPI(InputInfo inputInfo) throws Exception {
+        boolean facHasAvailability = false; // flips to yes if availability is found at any campsite at facility
+        URL availabilityUrl = null;
+        ObjectMapper availabilityMapper = new ObjectMapper();
+        CampgroundAvailability campgroundAvailability = null;
+        try {
+            availabilityUrl = new URL(createRgAvailApiString(inputInfo));
+            campgroundAvailability = availabilityMapper.readValue(availabilityUrl, CampgroundAvailability.class);
+        } catch (Exception ex) {
+            throw new Exception();
+        }
+
+        // go through all campsites at facility to check for availability
+        for (Campsite c : campgroundAvailability.getCampsites().values()) {
+            LocalDate dateHolder = inputInfo.getCheckInDate();
+            // cycle through all dates to check for availability across all nights, no need to check on check-out day
+            while (dateHolder.compareTo(inputInfo.getCheckOutDate()) < 0) {
+                String dateKey = createRgDateString(dateHolder);
+                String availabilityStatus;
+                try {
+                    availabilityStatus = c.getAvailabilities().get(dateKey).toString();
+                } catch (NullPointerException np) {
+                    availabilityStatus = "Unavailable"; // certain unavailable sites will not return any availability info so need to override that here
+                }
+                // break out of date availability loop if it is not available on one of the nights of the stays
+                if (!availabilityStatus.equals("Available")) {
+                    break;
+                }
+                // if the site is available and we have checked all dates
+                else if (dateHolder.compareTo(inputInfo.getCheckOutDate().minusDays(1)) == 0) {
+                    facHasAvailability = true;
+                }
+                dateHolder = dateHolder.plusDays(1);
+            }
+
+            // if a single campsite has availability, no need to check the rest
+            if (facHasAvailability) {
+                available = true;
+                break;
+            }
+        }
+
+        // if we make it through all campsites and there is no availability, add facility to unavailable list
+        if (!facHasAvailability) {
+            available = false;
+        }
+    }
+
+    private String createRgDateString(LocalDate dateHolder) {
+        StringBuilder stringBuilderDate = new StringBuilder();
+        stringBuilderDate.append(dateHolder.getYear());
+        stringBuilderDate.append("-");
+        stringBuilderDate.append(String.format("%02d", dateHolder.getMonthValue()));
+        stringBuilderDate.append("-");
+        stringBuilderDate.append(String.format("%02d", dateHolder.getDayOfMonth()));
+        stringBuilderDate.append("T00:00:00Z");
+        return stringBuilderDate.toString();
+    }
+
+    private String createRgAvailApiString(InputInfo inputInfo) {
+        // https://www.recreation.gov/api/camps/availability/campground/234718/month?start_date=2021-04-01T00%3A00%3A00.000Z
+        StringBuilder campgroundAvailabilityUrlSB = new StringBuilder();
+        campgroundAvailabilityUrlSB.append("https://www.recreation.gov/api/camps/availability/campground/");
+        campgroundAvailabilityUrlSB.append(rgFacilityId);
+        campgroundAvailabilityUrlSB.append("/month?start_date=");
+        campgroundAvailabilityUrlSB.append(inputInfo.getCheckInDate().getYear());
+        campgroundAvailabilityUrlSB.append("-");
+        campgroundAvailabilityUrlSB.append(String.format("%02d", inputInfo.getCheckInDate().getMonthValue()));
+        campgroundAvailabilityUrlSB.append("-01T00%3A00%3A00.000Z"); // this api call only accepts the first of the month
+        return campgroundAvailabilityUrlSB.toString();
+    }
+
+    public void callGoogPlaceAPI(String googleKey) throws Exception {
+        // google places API: pass in campsite name and lat long, receive google maps place information
+        URL facPlaceAPIUrl = null;
+        ObjectMapper facPlaceMapper = new ObjectMapper();
+        FacGooglePlace facGooglePlace = new FacGooglePlace();
+        try {
+            facPlaceAPIUrl = new URL(createGoogPlaceAPICallString(googleKey));
+            facGooglePlace = facPlaceMapper.readValue(facPlaceAPIUrl, FacGooglePlace.class);
+        } catch (Exception ex) {
+            // ok to come up empty here (if google can't find facility)
+        }
+        googRating = facGooglePlace.getResultsRating();
+        googName = facGooglePlace.getResultsName();
+        googPlaceId = facGooglePlace.getResultsPlace_id();
+        googUserRatingsTotal = facGooglePlace.getResultsUser_Ratings_Total();
+    }
+
+    private String createGoogPlaceAPICallString(String googleKey) {
+        // example call: "https://maps.googleapis.com/maps/api/place/textsearch/json?input=CAMP%20GATEWAY%20-%20SANDY%20HOOK&locationbias=point:40,-74&key=AIzaSyDjzILiKx-IzTpbnq7B9B21DV3a7KyeQZc"
+        StringBuilder facPlaceAPIUrlSB = new StringBuilder("https://maps.googleapis.com/maps/api/place/textsearch/json?input=");
+        facPlaceAPIUrlSB.append(rgFacilityName.replaceAll(" ", "%20"));
+        facPlaceAPIUrlSB.append("%20campground&locationbias=point:");
+        facPlaceAPIUrlSB.append(latitude);
+        facPlaceAPIUrlSB.append(",");
+        facPlaceAPIUrlSB.append(longitude);
+        facPlaceAPIUrlSB.append("&key=");
+        facPlaceAPIUrlSB.append(googleKey);
+        return facPlaceAPIUrlSB.toString();
+    }
 }
--- src/main/java/com/PersonalProjects/CampgroundFinder/InputController.java
+++ src/main/java/com/PersonalProjects/CampgroundFinder/InputController.java
@@ -48,99 +48,13 @@ public class InputController {
         for (Facility f : allFacs) {
             double dist = inputInfo.calcDistToFac(f); // calculate distance between input address and facility (campground)
             if (dist <= inputInfo.getRadius()) { // if facility (campground) is within radius, check for availability
-                boolean facHasAvailability = false; // flips to yes if availability is found at any campsite at facility
-
-                // https://www.recreation.gov/api/camps/availability/campground/234718/month?start_date=2021-04-01T00%3A00%3A00.000Z
-                StringBuilder campgroundAvailabilityUrlSB = new StringBuilder();
-                campgroundAvailabilityUrlSB.append("https://www.recreation.gov/api/camps/availability/campground/");
-                campgroundAvailabilityUrlSB.append(f.getRgFacilityId());
-                campgroundAvailabilityUrlSB.append("/month?start_date=");
-                campgroundAvailabilityUrlSB.append(inputInfo.getCheckInDate().getYear());
-                campgroundAvailabilityUrlSB.append("-");
-                campgroundAvailabilityUrlSB.append(String.format("%02d", inputInfo.getCheckInDate().getMonthValue()));
-                campgroundAvailabilityUrlSB.append("-01T00%3A00%3A00.000Z"); // this api call only accepts the first of the month
-
-                URL availabilityUrl = null;
-                ObjectMapper availabilityMapper = new ObjectMapper();
-                CampgroundAvailability campgroundAvailability = null;
-                try {
-                    availabilityUrl = new URL(campgroundAvailabilityUrlSB.toString());
-                    campgroundAvailability = availabilityMapper.readValue(availabilityUrl, CampgroundAvailability.class);
-                } catch (Exception ex) {
-                    model.addAttribute("availabilityapicallurl", campgroundAvailabilityUrlSB.toString());
-                    return "rgAPICallError";
-                }
-
-                // go through all campsites at facility to check for availability
-                for (Campsite c : campgroundAvailability.getCampsites().values()) {
-                    LocalDate dateHolder = inputInfo.getCheckInDate();
-                    // cycle through all dates to check for availability across all nights, no need to check on check-out day
-                    while (dateHolder.compareTo(inputInfo.getCheckOutDate()) < 0) {
-                        StringBuilder stringBuilderDate = new StringBuilder();
-                        stringBuilderDate.append(dateHolder.getYear());
-                        stringBuilderDate.append("-");
-                        stringBuilderDate.append(String.format("%02d", dateHolder.getMonthValue()));
-                        stringBuilderDate.append("-");
-                        stringBuilderDate.append(String.format("%02d", dateHolder.getDayOfMonth()));
-                        stringBuilderDate.append("T00:00:00Z");
-                        String dateKey = stringBuilderDate.toString();
-                        String availabilityStatus = new String();
-                        try {
-                            availabilityStatus = c.getAvailabilities().get(dateKey).toString();
-                        } catch (NullPointerException np) {
-                            availabilityStatus = "Unavailable"; // certain unavailable sites will not return any availability info so need to override that here
-                        }
-                        // break out of date availability loop if it is not available on one of the nights of the stays
-                        if (!availabilityStatus.equals("Available")) {
-                            break;
-                        }
-                        // if the site is available and we have checked all dates
-                        else if (dateHolder.compareTo(inputInfo.getCheckOutDate().minusDays(1)) == 0) {
-                            facHasAvailability = true;
-                        }
-                        dateHolder = dateHolder.plusDays(1);
-                    }
-
-                    // if a single campsite has availability, no need to check the rest
-                    if (facHasAvailability) {
-                        f.setAvailable(true);
-                        break;
-                    }
-                }
-
-                // if we make it through all campsites and there is no availability, add facility to unavailable list
-                if (!facHasAvailability) {f.setAvailable(false);}
-                facsInRad.add(f);
+                try {f.callAvailabilityAPI(inputInfo);}
+                catch (Exception ex) {return "rgAPICallError";}
+                facsInRad.add(f); // add facs to facsInRad after populating availability info (used by comparator)
+                try {f.callGoogPlaceAPI(googleKey);} // populate Google Place info for each facility in radius
+                catch (Exception ex) {return "googPlaceAPICallError";}
             }
         }
-
-        for (Facility f : facsInRad) {
-            // google places API: pass in campsite name and lat long, receive google maps place information
-            // example call: "https://maps.googleapis.com/maps/api/place/textsearch/json?input=CAMP%20GATEWAY%20-%20SANDY%20HOOK&locationbias=point:40,-74&key=AIzaSyDjzILiKx-IzTpbnq7B9B21DV3a7KyeQZc"
-            StringBuilder facPlaceAPIUrlSB = new StringBuilder("https://maps.googleapis.com/maps/api/place/textsearch/json?input=");
-            facPlaceAPIUrlSB.append(f.getRgFacilityName().replaceAll(" ", "%20"));
-            facPlaceAPIUrlSB.append("%20campground&locationbias=point:");
-            facPlaceAPIUrlSB.append(f.getLatitude());
-            facPlaceAPIUrlSB.append(",");
-            facPlaceAPIUrlSB.append(f.getLongitude());
-            facPlaceAPIUrlSB.append("&key=");
-            facPlaceAPIUrlSB.append(googleKey);
-            URL facPlaceAPIUrl = null;
-            ObjectMapper facPlaceMapper = new ObjectMapper();
-            FacGooglePlace facGooglePlace = new FacGooglePlace();
-            try {
-                facPlaceAPIUrl = new URL(facPlaceAPIUrlSB.toString());
-                facGooglePlace = facPlaceMapper.readValue(facPlaceAPIUrl, FacGooglePlace.class);
-            } catch (Exception ex) {
-                //return "Google Places API Call Error";
-                // ok to come up empty here
-            }
-            f.setGoogRating(facGooglePlace.getResultsRating());
-            f.setGoogName(facGooglePlace.getResultsName());
-            f.setGoogPlaceId(facGooglePlace.getResultsPlace_id());
-            f.setGoogUserRatingsTotal(facGooglePlace.getResultsUser_Ratings_Total());
-        }
-
         model.addAttribute("facsInRad",facsInRad);
         model.addAttribute("inputInfo", inputInfo);
         return "result";
